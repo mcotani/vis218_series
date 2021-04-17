323de59faacfb19ab9338f8a30750ab19851827b
Maria Clara Otani <31204684+mcotani@users.noreply.github.com>
2021-04-17 12:59:25 -0400

    major hanges

--- background.js
+++ background.js
@@ -1,66 +1,60 @@
+function resetBg(){
+    localStorage.clear();
+}
 function changeDivImage()
     {
         var imgPath = new String();
         imgPath = document.body.style.backgroundImage;
         console.log(imgPath);
         var bgs = ["url(images/home_vogue/pink_bg.png)", "url(images/home_vogue/marble_bg.png)", "url(images/home_vogue/yellow_bg.png)"];
-        // var max = 0;
-        // var min = 3;
-        // save data value
-        // localStorage.getItem("current", 0);
-
+       
         // retrieve data value
-        var currentItem = localStorage.getItem("current");
+        var currentItem = new Number;
+        currentItem = localStorage.getItem("current");
         console.log(currentItem)
-        if (currentItem == 01){
+        if (currentItem == null || currentItem ==7){
+            console.log("set first")
             localStorage.setItem("current", 0);
-            currentItem = 0;
+            document.body.style.backgroundImage = bgs[currentItem];
         }
-        document.body.style.backgroundImage = bgs[currentItem];
-        localStorage.getItem("current", currentItem+1);
-
-
-        // var random = Math.floor(Math.random() * (max - min) + min);
-
-        // console.log(random)
-
-        // document.body.style.backgroundImage = bgs[random];
-
-
-        // if(imgPath == "url(images/home_vogue/marble_bg.png)" || imgPath == "url(images/home_vogue/yellow_bg.png)" || imgPath == "")
-        // {
-        //     console.log("do pink")
-        //     document.body.style.backgroundImage = "url(images/home_vogue/pink_bg.png)";
-        // }
-        // else if (imgPath == "url(images/home_vogue/pink_bg.png)" || imgPath == "url(images/home_vogue/yellow_bg.png)")
-        // {
-            // console.log("do marble")
-            // else document.body.style.backgroundImage = "url(images/home_vogue/marble_bg.png)";
-        // }
-        // else if (imgPath == "url(images/home_vogue/marble_bg.png)" || imgPath == "url(images/home_vogue/pink_bg.png)")
-        // {
-        //     console.log("do yellow")
-
-        //     document.body.style.backgroundImage = "url(images/home_vogue/yellow_bg.png)";
-        // }
+        else{
+            document.body.style.backgroundImage = bgs[currentItem];
+            currentItem++;
+            localStorage.setItem("current", currentItem);
+        }
+        
     }
 function changeAvatar()
 {
     var imgPath = new String();
     imgPath = document.getElementById("avatarIm").src;
-    var url =  new URL(imgPath)
     console.log(imgPath);
-    console.log(url.href);
 
-    if(url == "url(images/home_vogue/feifei_avatar.png)" || imgPath == "")
-    {
-        console.log(" change avatar");
-        document.getElementById("avatarIm").imgPath = "url(images/home_vogue/grace_avatar.png)";
+    // var url =  new URL(imgPath)
+    // console.log(url.href);
+    // console.log(url);
+    // console.log();
+
+
+    // console.log(imgPath);
+    var avatars = ["images/home_vogue/feifei_avatar.png", "images/home_vogue/grace_avatar.png", "images/home_vogue/dude1_avatar.png", "images/home_vogue/dude2_avatar.png"];
+   
+    // retrieve data value
+    var currentItem = new Number;
+    currentItem = localStorage.getItem("currentAvatar");
+    console.log(currentItem)
+    if (currentItem == null || currentItem ==4){
+        console.log("set first")
+        localStorage.setItem("currentAvatar", 0);
+        currentItem = 0;
+        console.log(avatars[currentItem]);
+
+        document.getElementById("avatarIm").src = avatars[currentItem];
     }
-    else
-    {
-        console.log(" don't change avatar");
-
-        document.getElementById("div1").style.backgroundImage = "url(images/home_vogue/feifei_avatar.png)";
+    else{
+        document.getElementById("avatarIm").src = avatars[currentItem];
+        currentItem++;
+        localStorage.setItem("currentAvatar", currentItem);
     }
 }
+
