90ba3b5f82b006b3ee4693d6268f57115fda1c2c
Maria Clara Otani <31204684+mcotani@users.noreply.github.com>
2021-04-25 15:28:49 -0400

    pretty

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ function changeDivImage()
         var imgPath = new String();
         imgPath = document.body.style.backgroundImage;
         console.log(imgPath);
-        var bgs = [ "url(images/home_vogue/marble.png)", "url(images/home_vogue/yellow_bg.png)",  "url(images/home_vogue/flowers.jpeg)", "url(images/home_vogue/firework1.gif)", "url(images/home_vogue/gradient.jpeg)",  "url(images/home_vogue/grass.jpeg)"];
+        var bgs = [ "url(images/home_vogue/brocade7.jpeg)", "url(images/home_vogue/yellow_bg.png)",  "url(images/home_vogue/flowers.jpeg)", "url(images/home_vogue/firework1.gif)", "url(images/home_vogue/gradient.jpeg)",  "url(images/home_vogue/grass.jpeg)"];
        
         // retrieve data value
         var currentItem = new Number;
@@ -29,14 +29,6 @@ function changeAvatar()
     var imgPath = new String();
     imgPath = document.getElementById("avatarIm").src;
     console.log(imgPath);
-
-    // var url =  new URL(imgPath)
-    // console.log(url.href);
-    // console.log(url);
-    // console.log();
-
-
-    // console.log(imgPath);
     var avatars = ["images/home_vogue/feifei_avatar.png", "images/home_vogue/grace_avatar.png", "images/home_vogue/dude1_avatar.png", "images/home_vogue/dude2_avatar.png"];
    
     // retrieve data value
@@ -55,6 +47,32 @@ function changeAvatar()
         document.getElementById("avatarIm").src = avatars[currentItem];
         currentItem++;
         localStorage.setItem("currentAvatar", currentItem);
+    }
+}
+
+function changeDivBG()
+{
+    var imgPath = new String();
+    imgPath = document.getElementById("magazine").style.backgroundImage;
+    console.log(imgPath);
+    var bgs = [ "url(images/home_vogue/brocade7.jpeg)", "url(images/home_vogue/yellow_bg.png)",  "url(images/home_vogue/flowers.jpeg)", "url(images/home_vogue/firework1.gif)", "url(images/home_vogue/gradient.jpeg)",  "url(images/home_vogue/grass.jpeg)"];
+   
+    // retrieve data value
+    var currentItem = new Number;
+    currentItem = localStorage.getItem("currentMag");
+    console.log(currentItem)
+    if (currentItem == null || currentItem ==7){
+        console.log("set first")
+        localStorage.setItem("currentMag", 0);
+        currentItem = 0;
+        console.log(bgs[currentItem]);
+
+        document.getElementById("magazine").style.backgroundImage = bgs[currentItem];
+    }
+    else{
+        document.getElementById("magazine").style.backgroundImage = bgs[currentItem];
+        currentItem++;
+        localStorage.setItem("currentMag", currentItem);
     }
 }
 
